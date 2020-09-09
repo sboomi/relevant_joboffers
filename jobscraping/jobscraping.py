335b809bb07b8c4e6d4efae918a4af4fd259b9d0
Shadi Boomi <shadi.boomi@gmail.com>
2020-09-09 15:55:42 +0200

    [Scraping] Added script for Indeed with specific keywords

--- jobscraping/jobscraping.py
+++ jobscraping/jobscraping.py
@@ -5,38 +5,92 @@ from selenium.webdriver.support import expected_conditions as EC
 
 from bs4 import BeautifulSoup
 import requests
+import re
+from pymongo import MongoClient
+from geopy.geocoders import Nominatim
 
+#Load Database with testuser (read + write permissions)
+client = MongoClient("mongodb+srv://testuser:testuser@joboffers.jjm1u.mongodb.net/joboffers?retryWrites=true&w=majority")
+db = client.jobOffers
+collection = db.data
+
+#Load geolocator
+geolocator = Nominatim(user_agent="joboffers")
+
+#Using Indeed
 URL = "https://www.indeed.fr/"
 keyword_request = ["python", "r", "tensorflow"]
+n_pages = 5
 
 # Takes the URL from Indeed and sorts results by date
 def get_request_url(url, keywords):
     return url + "/jobs?q=" + "+".join(keywords) + "&sort=date"
+
+# Evaluates the frequency of a list of keywords and returns a dictionnary
+def kw_freq(keywords,text):
+    d_words = {keyword.lower(): 0 for keyword in keywords}
+    for word in re.split(r'\W', text):
+        if word.lower() in d_words:
+            d_words[word.lower()] += 1
+    return d_words
 
 browser = webdriver.Firefox()
 url_req = get_request_url(URL, keyword_request) 
 print(f"Accessing {url_req}...")
 browser.get(url_req)
 
-try:
-    page_scroll = WebDriverWait(browser, 30).until(EC.presence_of_element_located((By.CSS_SELECTOR, "div.pagination")))
-except:
-    print("Error: couldn't find page footer")
-    browser.quit()
+for i in range(1,n_pages+1):
+    try:
+        page_scroll = WebDriverWait(browser, 30).until(EC.presence_of_element_located((By.CSS_SELECTOR, "div.pagination")))
+    except:
+        print("Error: couldn't find page footer")
+        browser.quit()
 
-browser.execute_script("window.scrollTo(0,document.body.scrollHeight);")
+    browser.execute_script("window.scrollTo(0,document.body.scrollHeight);")
+    
+    try:
+        job_offers = browser.find_elements_by_css_selector("h2.title")
+    except:
+        print("Something went wrong. Quitting...")
+        browser.quit()
 
-job_offers = browser.find_elements_by_css_selector("h2.title")
+    for offer in job_offers:
+        offer_doc = {}
+        offer_url = offer.find_element_by_tag_name('a')
+        url = offer_url.get_attribute("href")
+        page_content = requests.get(url).content
+        soup = BeautifulSoup(page_content, "html.parser")
+        print(f"{url} parsed!")
+        offer_doc['_id'] = "INDEED_"+url.split("fccid=")[-1].replace("&vjs=3","")
+        offer_doc['url'] = url
+        offer_doc['title'] = soup.title.text
+        details = soup.find("div",class_="jobsearch-InlineCompanyRating")
+        if details:
+            offer_doc["company"] = details.select_one("div.icl-u-lg-mr--sm.icl-u-xs-mr--xs").text
+            city = details.find_all(class_=False)[-1].text
+            country_code = soup.select_one("html").get('lang')
+            location = geolocator.geocode(city, country_codes=country_code)
+            offer_doc['address'] = location.address
+        offer_doc["description"] = soup.find("div",id="jobDescriptionText").prettify()
+        offer_doc["job_type"] = soup.select_one("div.jobsearch-JobMetadataHeader-item").text if soup.select_one("div.jobsearch-JobMetadataHeader-item") else None
+        offer_doc["keyword_frequency"] = kw_freq(keyword_request, soup.find("div",id="jobDescriptionText").text)
+        
+        try:
+            post_id = collection.insert_one(offer_doc).inserted_id
+        except:
+            post_id = offer_doc["_id"]
+            print(f"Replacing  {post_id}...")
+            collection.replace_one({"_id": post_id},offer_doc)
+        else:
+            print(f"{post_id} inserted!")
 
-soup_list = []
 
-for offer in job_offers:
-    offer_url = offer.find_element_by_name('a')
-    page_content = requests.get(offer_url.get_attribute("href")).content
-    soup = BeautifulSoup(html_doc, "html.parser")
-    print(f"{offer.find_element_by_name('h2').text} parsed!")
-    soup_list.append(soup)
 
+    
+    print(f"Accessing page {i+1}...")
+    
+    browser.get(url_req + f"&start={i*10}")
+    
 
 
 browser.quit()
