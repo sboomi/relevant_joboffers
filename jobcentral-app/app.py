f2530d6de1568fbf4a3ed40863f8bec02068317c
sboomi <47070729+sboomi@users.noreply.github.com>
2020-09-09 22:06:13 +0200

    [App] WTForm extablished

--- jobcentral-app/app.py
+++ jobcentral-app/app.py
@@ -1,9 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from flask_pymongo import PyMongo
+
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb+srv://testuser:testuser@joboffers.jjm1u.mongodb.net/jobOffers?retryWrites=true&w" \
+                          "=majority "
+mongo = PyMongo(app)
+
+
+class SearchForm(FlaskForm):
+    query = StringField('Enter keywords')
+    submit = SubmitField('Search')
+
 
 @app.route('/')
+@app.route("/main")
 def hello_world():
-    return 'Hello World!'
+    latest_jobs = mongo.db.data.find().limit(10)
+    form = SearchForm()
+    return render_template("main.html", latest_jobs=latest_jobs, form=form)
+
 
 if __name__ == '__main__':
     app.run()
